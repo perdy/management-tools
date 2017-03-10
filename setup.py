84aec8b64dbf12414c2b9464fac7492d25705f59
Jose Antonio Perdiguero <perdy.hh@gmail.com>
2017-03-10 13:36:37 +0100

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ _KEYWORDS = ' '.join([
 
 setup(
     name='management-tools',
-    version='0.2.0',
+    version='0.3.0',
     description='A set of tools to measure and improve efficiency in team management tasks.',
     long_description=_LONG_DESCRIPTION,
     author='José Antonio Perdiguero López',
